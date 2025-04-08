7b4052b773b66b94ae0468269617b2e19ba5453b
lucas oliveira <lucashbo321@gmail.com>
2025-04-07 21:44:10 -0300

    remoção api/dist

--- API/dist/routes/taskRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getTasks, addTask, updateTask, deleteTask } from '../controllers/taskControllers.js';
-const taskRouter = express.Router();
-taskRouter.post('/add', addTask);
-taskRouter.get('/tasks', getTasks);
-taskRouter.put('/update', updateTask);
-taskRouter.delete('/delete', deleteTask);
-export default taskRouter;
