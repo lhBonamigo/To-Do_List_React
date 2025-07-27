350b8a0adfadad459556779906444f48ba6030f1
lucas bonamigo <lucasbo321@gmail.com>
2025-07-27 10:19:57 -0300

    teste

--- API/dist/middlewares/errorHancler.js
+++ API/dist/middlewares/errorHancler.js
@@ -1,4 +1,7 @@
 export const errorHandler = (err, req, res, next) => {
     console.error("Erro:", err); // Mostra o erro no console
-    res.status(500).json({ erro: "Erro interno do servidor" }); // Responde com erro padrão
+    res.status(500).json({
+        erro: "Erro interno do servidor",
+        Erro: err
+    });
 };
--- API/src/middlewares/errorHancler.ts
+++ API/src/middlewares/errorHancler.ts
@@ -2,5 +2,9 @@ import { Request, Response, NextFunction } from "express";
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   console.error("Erro:", err); // Mostra o erro no console
-  res.status(500).json({ erro: "Erro interno do servidor" }); // Responde com erro padrão
+  res.status(500).json(
+    {
+      erro: "Erro interno do servidor",
+      Erro: err
+    });
 };
